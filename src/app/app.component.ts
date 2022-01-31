723c6004c922bb22453524494bb2f4a3a4d61299
15971595MoBro <15971595MoBro@motaahtabro1597@gmail.com>
2022-01-31 18:13:34 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'login-register';
+  log_email: string;
+  log_password: string;
+  reg_email: string;
+  reg_password: string;
+  reg_confirm_password: string;
+
+  register() {}
+  login() {}
 }
